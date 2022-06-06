dac9c36e1d67681a393d05672fe69492c9a85a69
Oleg <taurusx85@gmail.com>
2022-06-06 14:16:59 +0300

    Initial commit

---
+++ src/main/java/com/taurusx85/App.java
@@ -0,0 +1,14 @@
+package com.taurusx85;
+
+
+public class App {
+
+    public static void main(String[] args) {
+       print("Hello, World!");
+    }
+
+
+    private static void print(String s) {
+        System.out.println(s);
+    }
+}
